8cb68a43e222549224a2d0741c51a86f63e682d5
mattallan <matt@prospress.com>
2020-07-06 16:08:55 +1000

    Update the default docker init file

--- tests/e2e/docker/initialize.sh
+++ tests/e2e/docker/initialize.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-echo "Initializing WooCommerce E2E"
+echo "Updating permalink structure"
+wp rewrite structure '/%postname%/'
 
+echo "Initializing WooCommerce E2E"
 wp plugin install woocommerce --activate
-wp theme install twentynineteen --activate
+wp theme install storefront --activate
 wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer --path=/var/www/html
+
+echo "Adding basic WooCommerce settings..."
+wp option set woocommerce_store_address "60 29th Street"
+wp option set woocommerce_store_address_2 "#343"
+wp option set woocommerce_store_city "San Francisco"
+wp option set woocommerce_default_country "US:CA"
+wp option set woocommerce_store_postcode "94110"
+wp option set woocommerce_currency "USD"
+wp option set woocommerce_product_type "both"
+wp option set woocommerce_allow_tracking "no"
+
+echo "Importing WooCommerce shop pages..."
+wp wc --user=admin tool run install_pages
+
+
+
+echo "Installing and activating the WordPress Importer plugin..."
+wp plugin install wordpress-importer --activate
+
+echo "Importing some sample data..."
+wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
