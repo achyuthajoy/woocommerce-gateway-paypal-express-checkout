d290d357e992977634dec3178d8737b0d315a876
Achyuth Ajoy <achyuthajoy@gmail.com>
2020-07-09 15:33:21 +0530

    Check TRAVIS Commit Range

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ${RUN_PHPCS} == 1 ]]; then
+	echo $TRAVIS_COMMIT_RANGE
 	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
 	IGNORE=""
 
